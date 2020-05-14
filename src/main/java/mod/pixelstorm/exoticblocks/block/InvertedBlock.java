a3f2707b3d82168a20fb3872aae1326c18f0f830
Pixelstorm <thepixelstormer@gmail.com>
2020-05-14 19:27:22 +0100

    Fix different coloured InvertedBlocks face interactions

--- src/main/java/mod/pixelstorm/exoticblocks/block/InvertedBlock.java
+++ src/main/java/mod/pixelstorm/exoticblocks/block/InvertedBlock.java
@@ -3,13 +3,14 @@ package mod.pixelstorm.exoticblocks.block;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
-import net.minecraft.block.TransparentBlock;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 
-public class InvertedBlock extends TransparentBlock
+public class InvertedBlock extends Block
 {
 	public static final FabricBlockSettings DEFAULT_SETTINGS = FabricBlockSettings.of(Material.STONE).strength(1.8F).nonOpaque();
 
@@ -27,5 +28,11 @@ public class InvertedBlock extends TransparentBlock
 	public float getAmbientOcclusionLightLevel(BlockState state, BlockView view, BlockPos pos)
 	{
 		return 1.0F;
+	}
+
+	@Environment(EnvType.CLIENT)
+	public boolean isSideInvisible(BlockState state, BlockState neighbor, Direction facing)
+	{
+		return (neighbor.getBlock() instanceof InvertedBlock) ? true : super.isSideInvisible(state, neighbor, facing);
 	}
 }
