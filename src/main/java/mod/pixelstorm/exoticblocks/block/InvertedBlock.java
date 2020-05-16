79098a0928d13f6f5eb499aa29b9b98e3c26be87
Pixelstorm <thepixelstormer@gmail.com>
2020-05-16 13:49:26 +0100

    Fix inverted block rendering

--- src/main/java/mod/pixelstorm/exoticblocks/block/InvertedBlock.java
+++ src/main/java/mod/pixelstorm/exoticblocks/block/InvertedBlock.java
@@ -3,16 +3,21 @@ package mod.pixelstorm.exoticblocks.block;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.block.FabricMaterialBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
+import net.minecraft.block.MaterialColor;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 public class InvertedBlock extends Block
 {
-	public static final FabricBlockSettings DEFAULT_SETTINGS = FabricBlockSettings.of(Material.STONE).strength(1.8F, 1.8F);
+	public static final FabricBlockSettings DEFAULT_SETTINGS = FabricBlockSettings.of(new FabricMaterialBuilder(MaterialColor.STONE).requiresTool().lightPassesThrough().build()).strength(1.8F, 1.8F);
 
 	public InvertedBlock()
 	{
@@ -24,12 +29,21 @@ public class InvertedBlock extends Block
 		super(settings);
 	}
 
+	@Override
+	@Environment(EnvType.CLIENT)
+	public RenderLayer getRenderLayer()
+	{
+		return RenderLayer.TRANSLUCENT;
+	}
+
+	@Override
 	@Environment(EnvType.CLIENT)
 	public float getAmbientOcclusionLightLevel(BlockState state, BlockView view, BlockPos pos)
 	{
 		return 1.0F;
 	}
 
+	@Override
 	@Environment(EnvType.CLIENT)
 	public boolean isSideInvisible(BlockState state, BlockState neighbor, Direction facing)
 	{
