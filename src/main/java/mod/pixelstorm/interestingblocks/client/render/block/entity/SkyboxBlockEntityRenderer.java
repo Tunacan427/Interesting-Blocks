038b03fc825b9ca194bd271d409a83aa6e5dd5f6
Pixelstorm <thepixelstormer@gmail.com>
2020-05-27 22:11:57 +0100

    Add face culling to skybox block

--- src/main/java/mod/pixelstorm/interestingblocks/client/render/block/entity/SkyboxBlockEntityRenderer.java
+++ src/main/java/mod/pixelstorm/interestingblocks/client/render/block/entity/SkyboxBlockEntityRenderer.java
@@ -1,5 +1,6 @@
 package mod.pixelstorm.interestingblocks.client.render.block.entity;
 
+import java.util.function.Function;
 import mod.pixelstorm.interestingblocks.block.entity.SkyboxBlockEntity;
 import mod.pixelstorm.interestingblocks.client.render.SkyboxTexturing;
 import mod.pixelstorm.interestingblocks.client.texture.SkyboxBlockTexture;
@@ -12,6 +13,7 @@ import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.util.math.Matrix4f;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Direction;
 
 public class SkyboxBlockEntityRenderer extends BlockEntityRenderer<SkyboxBlockEntity>
 {
@@ -25,30 +27,33 @@ public class SkyboxBlockEntityRenderer extends BlockEntityRenderer<SkyboxBlockEn
 	@Override
 	public void render(SkyboxBlockEntity blockEntity, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, int overlay)
 	{
-		renderBlock(matrixStack, vertexConsumerProvider);
+		renderBlock(matrixStack, vertexConsumerProvider, blockEntity::shouldDrawSide);
 	}
 
-	public static void renderBlock(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider)
+	public static void renderBlock(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, Function<Direction, Boolean> shouldDrawSide)
 	{
 		SkyboxBlockTexture.getInstance().getFramebuffer().checkFramebufferStatus();
-		renderCube(matrixStack.peek().getModel(), vertexConsumerProvider.getBuffer(SKYBOX_RENDERLAYER));
+		renderCube(matrixStack.peek().getModel(), vertexConsumerProvider.getBuffer(SKYBOX_RENDERLAYER), shouldDrawSide);
 	}
 
-	private static void renderCube(Matrix4f matrix, VertexConsumer vertexConsumer)
+	private static void renderCube(Matrix4f matrix, VertexConsumer vertexConsumer, Function<Direction, Boolean> shouldDrawSide)
 	{
-		renderFace(matrix, vertexConsumer, 0, 1, 1, 0, 0, 0, 0, 0); // Direction.NORTH
-		renderFace(matrix, vertexConsumer, 1, 1, 1, 0, 0, 1, 1, 0); // Direction.EAST
-		renderFace(matrix, vertexConsumer, 0, 1, 0, 1, 1, 1, 1, 1); // Direction.SOUTH
-		renderFace(matrix, vertexConsumer, 0, 0, 0, 1, 0, 1, 1, 0); // Direction.WEST
-		renderFace(matrix, vertexConsumer, 0, 1, 0, 0, 0, 0, 1, 1); // Direction.DOWN
-		renderFace(matrix, vertexConsumer, 0, 1, 1, 1, 1, 1, 0, 0); // Direction.UP
+		renderFace(matrix, vertexConsumer, 0, 1, 1, 0, 0, 0, 0, 0, Direction.NORTH, shouldDrawSide);
+		renderFace(matrix, vertexConsumer, 1, 1, 1, 0, 0, 1, 1, 0, Direction.EAST, shouldDrawSide);
+		renderFace(matrix, vertexConsumer, 0, 1, 0, 1, 1, 1, 1, 1, Direction.SOUTH, shouldDrawSide);
+		renderFace(matrix, vertexConsumer, 0, 0, 0, 1, 0, 1, 1, 0, Direction.WEST, shouldDrawSide);
+		renderFace(matrix, vertexConsumer, 0, 1, 0, 0, 0, 0, 1, 1, Direction.DOWN, shouldDrawSide);
+		renderFace(matrix, vertexConsumer, 0, 1, 1, 1, 1, 1, 0, 0, Direction.UP, shouldDrawSide);
 	}
 
-	private static void renderFace(Matrix4f matrix, VertexConsumer vertexConsumer, float x1, float x2, float y1, float y2, float z1, float z2, float z3, float z4)
+	private static void renderFace(Matrix4f matrix, VertexConsumer vertexConsumer, float x1, float x2, float y1, float y2, float z1, float z2, float z3, float z4, Direction direction, Function<Direction, Boolean> shouldDrawSide)
 	{
-		vertexConsumer.vertex(matrix, x1, y1, z1).color(255, 255, 255, 255).next();
-		vertexConsumer.vertex(matrix, x2, y1, z2).color(255, 255, 255, 255).next();
-		vertexConsumer.vertex(matrix, x2, y2, z3).color(255, 255, 255, 255).next();
-		vertexConsumer.vertex(matrix, x1, y2, z4).color(255, 255, 255, 255).next();
+		if(shouldDrawSide.apply(direction))
+		{
+			vertexConsumer.vertex(matrix, x1, y1, z1).color(255, 255, 255, 255).next();
+			vertexConsumer.vertex(matrix, x2, y1, z2).color(255, 255, 255, 255).next();
+			vertexConsumer.vertex(matrix, x2, y2, z3).color(255, 255, 255, 255).next();
+			vertexConsumer.vertex(matrix, x1, y2, z4).color(255, 255, 255, 255).next();
+		}
 	}
 }
